5f8a93b7788f7955ec97c884cc2d2bcaf2a0abeb
Anna <anya.naumova.1994@gmail.com>
2022-10-23 18:06:46 +0300

    last commit

--- script/circle.js
+++ script/circle.js
@@ -2,6 +2,6 @@ const texts = document.querySelectorAll(".slider__circle-paragraph");
 
 for (let text of texts) {
   text.innerHTML = text.innerText.split("").map((letter, i) =>
-    `<span style="transform:rotate(${i * 8.2}deg")>${letter}</span>`
+    `<span style="transform: rotate(${i * 8.2}deg")>${letter}</span>`
   ).join("");
 }
